41de89c815e6b6dd1bcb4d533bb8833d0b8caddc
HanLuca <hanluca2022@gmail.com>
2024-08-20 13:22:32 +0900

    First

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+	name='easyfirebase',
+	version='1.0.0',
+	description='use easy firebase!',
+	license='MIT',
+    install_requires=[],
+	packages=['easyfirebase'],
+	author='HanLuca',
+	author_email='hanluca2022@gmail.com',
+	keywords=['firebase', 'easy', 'easyfirebase', 'database', 'data'],
+	url='https://github.com/HanLuca/easyfirebase'
+)
